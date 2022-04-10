7d73b1ccd2baa2eee8e4ed41eeb24243935a49b6
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-04-11 01:41:05 +0700

    style: card products

--- home.php
+++ home.php
@@ -68,14 +68,29 @@ if (isset($_POST['add_to_cart'])) {
             while ($fetch_products = mysqli_fetch_assoc($select_products)) {
          ?>
                <form action="" method="post" class="box">
-                  <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-                  <div class="name"><?php echo $fetch_products['name']; ?></div>
-                  <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-                  <input type="number" min="1" name="product_quantity" value="1" class="qty">
-                  <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-                  <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+                  <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>" class="price">
                   <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-                  <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+
+                  <div class="image">
+                     <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+                  </div>
+                  <div class="details">
+                     <div class="name">
+                        <img src="./public/card/name.svg" alt="name_icon">
+                        <?php echo $fetch_products['name']; ?>
+                     </div>
+                     <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+                     <div class="qty-pri">
+                        <input type="number" min="1" name="product_quantity" value="1" class="qty">
+                        <div class="price">
+                           $<?php echo $fetch_products['price']; ?>/-
+                        </div>
+                     </div>
+                     <div class="action">
+                        <button type="submit" name="add_to_cart"><img src="./public/card/cart.svg" alt="cart_icon">add to cart</button>
+                        <img src="./public/card/heart.svg" alt="favourite_icon">
+                     </div>
+                  </div>
                </form>
          <?php
             }
