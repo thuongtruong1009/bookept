e293cd326dd8f4f88f64a62816a9842ae49f1249
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-04-11 20:24:14 +0700

    add more book

--- home.php
+++ home.php
@@ -64,7 +64,7 @@ if (isset($_POST['add_to_cart'])) {
       <h1 class="title">latest products</h1>
       <div class="box-container">
          <?php
-         $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+         $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 8") or die('query failed');
          if (mysqli_num_rows($select_products) > 0) {
             while ($fetch_products = mysqli_fetch_assoc($select_products)) {
          ?>
