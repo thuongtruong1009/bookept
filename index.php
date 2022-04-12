c5803d859f63add680597e75954fd65580838826
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-04-12 11:55:22 +0700

    feat(add): index page layout

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookist</title>
+    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <nav>
+        <div>
+            <h2>Bookist</h2>
+        </div>
+        <div class="link">
+            <a href="admin_page.php">
+                <h3>Admin page</h3>
+            </a>
+            <img src="./public/index/github.svg" alt="github">
+        </div>
+    </nav>
+    <main>
+        <div class="content">
+            <div>
+                <h3>Bookist: a dead simple reading list. Search books, add them to your Bookist and read them when you're ready. Once you finish the book, mark it read and watch your bookshelf stack up!</h3>
+                <a href="home.php">
+                <i class="fa-solid fa-compass"></i>
+                    <h3>Explore</h3>
+                </a>
+            </div>
+        </div>
+        <div class="sample">
+            <img src="./public/index/iphone.png" alt="iphone_img">
+            <div></div>
+        </div>
+    </main>
+</body>
+
+</html>
