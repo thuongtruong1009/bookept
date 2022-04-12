c91fc4a96023f5c8db0296de9ba8588490aebce3
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-04-12 12:09:27 +0700

    add desciption

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bookist</title>
+    <meta name="description" content="Knowledge space for nerds. Search online books by subject and add them to your favorite cart">
+    <meta name="keywords" content="php, sql, mysql, html, css, javascript, book">
     <link rel="shortcut icon" href="./public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./styles/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
