91e3eb323b53a53a478e517fcf81ffcc9c76d32b
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-04-12 12:01:58 +0700

    add logo-favicon

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bookist</title>
+    <link rel="shortcut icon" href="./public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./styles/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <nav>
-        <div>
+        <div class="logo">
+            <img src="./public/favicon.ico" alt="">
             <h2>Bookist</h2>
         </div>
         <div class="link">
