bf5d74802de877fc5339c271c639098dcca4cd19
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-05-27 18:41:52 +0700

    setup remote sql

--- config.php
+++ config.php
@@ -1,3 +1,6 @@
 <?php
+//Development
+// $conn = mysqli_connect('localhost:3325', 'root', '', 'shop_db') or die('connection failed');
+//Production
+$conn = mysqli_connect('remotemysql.com', 'V0NY1PkAnT', 'DhEl6P3aua', 'V0NY1PkAnT') or die('connection failed');
 
-$conn = mysqli_connect('localhost:3325', 'root', '', 'shop_db') or die('connection failed');
